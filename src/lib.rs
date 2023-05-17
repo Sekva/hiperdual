b0b53c9dff6e02ae641c805cab789bc6c38ea513
Matheus Machado Vieira <matheusmachado708@gmail.com>
2023-05-16 23:29:38 -0300

    Adicionando o struct só pra constar

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct HDNumber {
+    real: f64,
+    eps_1: f64,
+    eps_2: f64,
+    eps_12: f64,
+}
+
+impl HDNumber {
+    pub fn new(real: f64, eps_1: f64, eps_2: f64, eps_12: f64) -> HDNumber {
+        HDNumber {
+            real,
+            eps_1,
+            eps_2,
+            eps_12,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +22,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let x = HDNumber::new(1.0, 0.0, 0.0, 1.0);
+        assert_eq!(x.real, 1.0);
+        assert_eq!(x.eps_1, 0.0);
+        assert_eq!(x.eps_2, 0.0);
+        assert_eq!(x.eps_12, 1.0);
     }
 }
